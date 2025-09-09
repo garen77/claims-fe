70800206fcd94fb6534760a3755cf0ffd4ee7bb0
creo.cons <creo.cons@bene.it>
2025-09-09 22:11:09 +0200

    frer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,9 @@
 import { Component, signal } from '@angular/core';
-<<<<<<< HEAD
-import { RouterOutlet,RouterLink } from '@angular/router';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet,RouterLink],
-=======
 import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink],
->>>>>>> 7e5a02d15d027072564431daf0cc8cb3a8f013b8
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
